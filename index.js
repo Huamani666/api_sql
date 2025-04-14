3067b66bd6e5cf0eca40f669d20ba1eda7979f88
Huamani666 <i1115038@continental.edu.pe>
2025-04-14 14:27:05 -0500

    se sube el proyecto

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const sql = require('mssql');
+const app = express();
+const PORT = 3000;
+
+// Configuración de la conexión
+const config = {
+    user: 'sa',
+    password: '123456',
+    server: 'DESKTOP-HBV87UV', // o IP de tu servidor
+    database: 'RestauranteDb',
+    tabla: 'Productos',
+    options: {
+        encrypt: true, // si usas Azure; pon false si es local
+        trustServerCertificate: true // true si usas SQL Server local sin certificado
+    }
+};
+
+// Ruta GET para obtener datos
+app.get('/Productos', async (req, res) => {
+    try {
+        await sql.connect(config);
+        const result = await sql.query('SELECT * FROM Productos');
+        res.json(result.recordset);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error en la conexión a la base de datos');
+    }
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
